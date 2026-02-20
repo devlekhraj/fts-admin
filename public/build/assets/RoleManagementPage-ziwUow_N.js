35ebcb2857262e3aaf1f97792986f8841eb48772
devlekhraj <devlekhraj88@gmail.com>
2026-02-21 02:37:49 +0545

    admin crud with delete

--- public/build/assets/RoleManagementPage-ziwUow_N.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./AppPageHeader.vue_vue_type_script_setup_true_lang-d7f79ynP.js";import{d as a,T as t,p as n}from"./main-Dm920kri.js";const i=a({__name:"RoleManagementPage",setup(o){return(s,r)=>(n(),t(e,{title:"Role Management",subtitle:"Manage roles and their permissions"}))}});export{i as default};
