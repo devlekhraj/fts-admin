dff774e08322c27e20c4d852db625e2d7cd43b86
devlekhraj <devlekhraj88@gmail.com>
2026-05-26 10:51:27 +0545

    table - order_items, product_name length increased from 191 to 500

---
+++ database/migrations/2026_05_26_103428_alter_product_name_length_in_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            if(!Schema::hasColumn('order_items', 'product_name')) {
+                $table->string('product_name', 500)->after('product_id');
+            }
+            if(Schema::hasColumn('order_items', 'product_name')) {
+                $table->string('product_name', 500)->change();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            if(Schema::hasColumn('order_items', 'product_name')) {
+                $table->string('product_name', 500)->change();
+            }
+        });
+    }
+};
